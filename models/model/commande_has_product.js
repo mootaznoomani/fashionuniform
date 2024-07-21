05f877c06f49b06a25823ce969cec54480fb9e6b
mootaznoomani <91527816+mootaznoomani@users.noreply.github.com>
2024-07-21 13:54:08 +0100

    deploy

--- models/model/commande_has_product.js
+++ models/model/commande_has_product.js
@@ -1,24 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Commande_has_Product', {
-      Commande_idCommande: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        references: {
-          model: 'Commande',
-          key: 'idCommande'
-        }
-      },
-      Product_idProduct: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        references: {
-          model: 'Product',
-          key: 'idProduct'
-        }
+  return sequelize.define('Commande_has_Product', {
+    Commande_idCommande: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Commande',
+        key: 'idCommande'
       }
-    }, {
-      tableName: 'Commande_has_Product',
-      timestamps: false
-    });
-  };
-  
+    },
+    Product_idProduct: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'idProduct'
+      }
+    }
+  }, {
+    tableName: 'Commande_has_Product',
+    timestamps: false,
+    indexes: [
+      {
+        unique: true,
+        fields: ['Commande_idCommande', 'Product_idProduct'],
+        name: 'unique_cmd_prod' // Use a shorter name here
+      }
+    ]
+  });
+};
